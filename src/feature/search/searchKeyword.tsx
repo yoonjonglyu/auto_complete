64407f56b9b9f2bbf557a2894d17df39500ec2ab
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-23 20:07:41 +0900

    feat(FEATURE): SEARCH KEYWORD - 바인드 키워드 상태

--- src/feature/search/searchKeyword.tsx
+++ src/feature/search/searchKeyword.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+
+import searchKeywordState from '../../store/searchkeyword';
 
 interface SearchKeywordProps {}
 
 const SearchKeywordForm: React.FC<SearchKeywordProps> = () => {
-  return <section><ul>
-    <li>1</li></ul></section>;
+  const [searchKeyword, setSearchKeyword] = useRecoilState(searchKeywordState);
+
+  return (
+    <section>
+      <ul>
+        {searchKeyword.map((keyword, idx) => (
+          <li key={idx}>{keyword}</li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default SearchKeywordForm;
