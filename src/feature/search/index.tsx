088ba3bc59b00c2fec4b529eb4813eaca8ef18cc
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-23 20:10:08 +0900

    feat(FEATURE): SEARCH FEATURE - 현재 선택된 검색어 상태

--- src/feature/search/index.tsx
+++ src/feature/search/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SearchForm from './searchForm';
 import SearchKeyword from './searchKeyword';
@@ -6,6 +6,12 @@ import SearchKeyword from './searchKeyword';
 interface SearchFeatureProps {}
 
 const SearchFeature: React.FC<SearchFeatureProps> = () => {
+  const [currentKeyword, setCurrentKeyword] = useState(0);
+
+  const handleCurrentIdx = (value: number) => {
+    setCurrentKeyword(value);
+  };
+
   return (
     <main>
       <h1>검색 콤보 박스</h1>
