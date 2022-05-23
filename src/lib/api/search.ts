3035595b4264316d644383231aa6d9b7277b1ba1
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-23 23:09:14 +0900

    feat(API): SEARCH API - 데이터 포맷 정리

--- src/lib/api/search.ts
+++ src/lib/api/search.ts
@@ -5,15 +5,15 @@ class HackerNewsApi {
   constructor() {
     this.apiUri = 'https://hn.algolia.com/api/v1/';
   }
-  async getSearchItem(id: string){
-    const res = await Axios.get(`${this.apiUri}item/${id}`);
+  async getSearchItem(id: string) {
+    const { data } = await Axios.get(`${this.apiUri}items/${id}`);
 
-    return res;
+    return data;
   }
-  async getSearchKeyword(keyword: string){
-    const res = await Axios.get(`${this.apiUri}search?query=${keyword}`);
+  async getSearchKeyword(keyword: string) {
+    const { data } = await Axios.get(`${this.apiUri}search?query=${keyword}`);
 
-    return res;
+    return data.hits;
   }
 }
 
