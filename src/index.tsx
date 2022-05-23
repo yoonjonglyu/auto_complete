24dfb857e09d05cc840b2d6e40c2b08564dc450e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-23 16:08:56 +0900

    feat(ROOT): BIND ROOT - APP

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import App from './App';
+
 const root = document.querySelector('#app');
 if (root === null) {
   console.error('루트를 찾을 수 없습니다.');
 } else {
-  createRoot(root).render(<div>foo</div>);
+  createRoot(root).render(<App />);
 }
