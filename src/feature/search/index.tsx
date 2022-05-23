188f7f42486a7911677316353b45b06fa55396af
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-23 18:05:33 +0900

    feat(FEATURE): SEARCH - INIT

---
+++ src/feature/search/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface SearchFeatureProps{
+
+}
+
+const SearchFeature: React.FC<SearchFeatureProps> = () => {
+  return (
+    <div>search</div>
+  );
+}
+
+export default SearchFeature;
