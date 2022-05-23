0f63dcc0661e5138984034e76878b393996bd452
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-23 23:22:00 +0900

    feat(FEATURE): SEARCH FEATURE - 현재 검색어 함수 3가지 경우 추가

--- src/feature/search/index.tsx
+++ src/feature/search/index.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+
+import searchKeywordState from '../../store/searchkeyword';
 
 import SearchForm from './searchForm';
 import SearchKeyword from './searchKeyword';
@@ -7,9 +10,20 @@ interface SearchFeatureProps {}
 
 const SearchFeature: React.FC<SearchFeatureProps> = () => {
   const [currentKeyword, setCurrentKeyword] = useState(0);
-
-  const handleCurrentIdx = (value: number) => {
-    setCurrentKeyword(value);
+  const searchkeywords = useRecoilValue(searchKeywordState);
+  
+  const handleCurrentIdx = (value: 'up' | 'down' | 'reset') => {
+    if (value === 'reset') {
+      setCurrentKeyword(0);
+    } else if (value === 'up') {
+      setCurrentKeyword(prev =>
+        prev - 1 > -1 ? prev - 1 : searchkeywords.length - 1
+      );
+    } else {
+      setCurrentKeyword(prev =>
+        prev + 1 < searchkeywords.length ? prev + 1 : 0
+      );
+    }
   };
 
   return (
