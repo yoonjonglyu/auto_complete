a8dcf0604ed29cdcef22ab7c3591c97db264ebea
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-23 18:43:29 +0900

    feat(FEATURE): SEARCH FORM - 검색 이벤트 기본 틀

--- src/feature/search/searchForm.tsx
+++ src/feature/search/searchForm.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface SearchFormProps {}
 
 const SearchForm: React.FC<SearchFormProps> = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(inputValue);
+  };
+  const handleSearchKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+  const handleSelectItem = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'ArrowUp') {
+      console.log('up');
+    } else if (e.key === 'ArrowDown') {
+      console.log('down');
+    }
+  };
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleSearchKeyword}
+        onKeyUp={handleSelectItem}
+      />
     </form>
   );
 };
